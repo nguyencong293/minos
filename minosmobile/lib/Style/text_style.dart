3dcb8e4fee7673c8fd2884f1aae03a1157252d9e
nguyencong293 <nguyenthanhcong210903@gmail.com>
2024-09-27 00:22:19 +0700

    text style

---
+++ minosmobile/lib/Style/text_style.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle heading1 = TextStyle(
+    fontSize: 40.0,
+    fontFamily: 'Fz Poppins',
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle heading2 = TextStyle(
+    fontSize: 32.0,
+    fontFamily: 'Fz Poppins',
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle heading3 = TextStyle(
+    fontSize: 24.0,
+    fontFamily: 'Fz Poppins',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle heading4 = TextStyle(
+    fontSize: 20.0,
+    fontFamily: 'Fz Poppins',
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle subtitle = TextStyle(
+    fontSize: 20.0,
+    fontFamily: 'Fz Poppins',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle label = TextStyle(
+    fontSize: 16.0,
+    fontFamily: 'Fz Poppins',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle subBody = TextStyle(
+    fontSize: 20.0,
+    fontFamily: 'Fz Poppins',
+    fontWeight: FontWeight.w300,
+  );
+
+  static const TextStyle body = TextStyle(
+    fontSize: 16.0,
+    fontFamily: 'Fz Poppins',
+    fontWeight: FontWeight.w300,
+  );
+
+  static const TextStyle caption = TextStyle(
+    fontSize: 13.0,
+    fontFamily: 'Fz Poppins',
+    fontWeight: FontWeight.w300,
+  );
+}
